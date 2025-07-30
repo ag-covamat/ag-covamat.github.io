75504eb480fb58bd26f35f0a580ec4e84c903969
ag-covamat <a.grossmann@toutfaire.pro>
2025-07-30 16:30:09 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 const CORRECT_PASSWORD = "motdepassesupersecret";
 
+// Get references to the DOM elements
 const passwordOverlay = document.getElementById('password-overlay');
 const passwordInput = document.getElementById('password-input');
 const submitPasswordBtn = document.getElementById('submit-password');
@@ -7,6 +8,7 @@ const errorMessage = document.getElementById('error-message');
 const mainContent = document.getElementById('main-content');
 const logoutButton = document.getElementById('logout-button');
 
+// Function to check authentication status
 function checkAuthentication() {
     const isAuthenticated = localStorage.getItem('isAuthenticated');
     if (isAuthenticated === 'true') {
@@ -16,16 +18,19 @@ function checkAuthentication() {
     }
 }
 
+// Function to display the password prompt
 function showPasswordPrompt() {
     passwordOverlay.style.display = 'flex';
     mainContent.style.display = 'none';
 }
 
+// Function to display the main content
 function showContent() {
     passwordOverlay.style.display = 'none';
     mainContent.style.display = 'block';
 }
 
+// Function to handle password authentication
 function authenticate() {
     const enteredPassword = passwordInput.value;
     if (enteredPassword === CORRECT_PASSWORD) {
@@ -35,22 +40,23 @@ function authenticate() {
     } else {
         errorMessage.style.display = 'block';
     }
-    passwordInput.value = '';
+    passwordInput.value = ''; // Clear the password field after attempt
 }
 
+// Function to handle user logout
 function logout() {
     localStorage.removeItem('isAuthenticated');
     showPasswordPrompt();
 }
 
-// Écouteurs d'événements
+// Event Listeners
 submitPasswordBtn.addEventListener('click', authenticate);
-passwordInput.addEventListener('keypress', function (event) {
+passwordInput.addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         authenticate();
     }
 });
 logoutButton.addEventListener('click', logout);
 
-// Appelle la fonction de vérification au chargement de la page
+// Check authentication on page load
 document.addEventListener('DOMContentLoaded', checkAuthentication);
